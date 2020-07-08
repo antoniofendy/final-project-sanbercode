d4c9cd8dd67a7a3adcb6d16ccfb940fe42256b45
antoniofendy <layfenfen@gmail.com>
2020-07-08 22:09:22 +0700

    Adding new file for final project

---
+++ database/migrations/2020_07_08_093507_create_profile_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('telepon');
+            $table->text('alamat');
+            $table->unsignedBigInteger('user_id')->index();
+            
+            $table->foreign('user_id')->references('id')->on('users')
+            ->onDelete('cascade')
+            ->onUpdate('cascade')
+            ;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
