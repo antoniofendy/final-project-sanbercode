f2062360d32a05a8bb0edd8283e2b7209220c18e
antoniofendy <layfenfen@gmail.com>
2021-12-18 21:54:14 +0700

    Fixing TinyMCE

--- resources/views/user/komentar/komentar_jawaban.blade.php
+++ resources/views/user/komentar/komentar_jawaban.blade.php
@@ -23,44 +23,40 @@
 
 ?>
 
-
+<!DOCTYPE HTML>
 @extends('layouts.app')
-
-@section('navbar')
-    <nav class="navbar navbar-light">
-        <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
-        </form>
+< @section('navbar') <nav class="navbar navbar-light">
+    <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+    </form>
     </nav>
-@endsection
+    @endsection
 
 
-<style>
+    <style>
+        .card.main {
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        }
+    </style>
 
-    .card.main{
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    }
+    @section('content')
+    <div class="row p-2">
 
-</style>
-
-@section('content')
-<div class="row p-2">
-
-    <div class="col-md-2 mb-2">
-        @include('layouts.partials.leftbar')
-    </div>
-    <div class="col-md-8 mb-2">
+        <div class="col-md-2 mb-2">
+            @include('layouts.partials.leftbar')
+        </div>
+        <div class="col-md-8 mb-2">
             <div class="row justify-content-center">
                 <div class="col-md-12">
                     <div class="card main">
                         <div class="card-header"></div>
-                        
+
                         <div class="card-body">
                             {{-- @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
                             @endif --}}
                             <div class="card mb-2">
                                 <div class="card-header bg-warning">
@@ -69,46 +65,47 @@
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="col-md-10 col-sm-12">
-        
+
                                             <p p class="card-text">{!!$data_jawab->description!!}</p>
-                                            
+
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            
+
                         </div>
-                            
-                        </div>
+
                     </div>
                 </div>
-                <div class="card mt-2">
-                    <div class="card-header bg-info">
-                        Komentar Kamu
-                    </div>
-                    <div class="card-body">
-                        <form method="post" action="{{url('/komen-jawab')}}">
-                            @csrf
-                            <?php
+            </div>
+            <div class="card mt-2">
+                <div class="card-header bg-info">
+                    Komentar Kamu
+                </div>
+                <div class="card-body">
+                    <form method="post" action="{{url('/komen-jawab')}}">
+                        @csrf
+                        <?php
                                 $user = User::find(Auth::id());
                             ?>
-                            <input type="hidden" name="created_at" value="{{$current_date_time}}">
-                            <input type="hidden" name="updated_at" value="{{$current_date_time}}">
-                            <input type="hidden" name="user_id" value="{{$user->id}}">
-                            <input type="hidden" name="jawaban_id" value="{{$data_jawab->id}}">
-                            <input type="hidden" name="pertanyaan_id" value="{{$data_jawab->pertanyaan_id}}">
-                            <div class="form-group">
-                                <label for="isi"><b>Isi Komentar</b></label>
-                                <textarea style="height: 200px" name="isi" class="form-control my-editor">{!! old('isi', $isi ?? '') !!}</textarea>
-                            </div>
-                            <div class="form-group text-center">
-                                <button type="submit" class="btn btn-outline-primary">Submit komentar</button>
-                            </div>
-                        </form>
-                    </div>
+                        <input type="hidden" name="created_at" value="{{$current_date_time}}">
+                        <input type="hidden" name="updated_at" value="{{$current_date_time}}">
+                        <input type="hidden" name="user_id" value="{{$user->id}}">
+                        <input type="hidden" name="jawaban_id" value="{{$data_jawab->id}}">
+                        <input type="hidden" name="pertanyaan_id" value="{{$data_jawab->pertanyaan_id}}">
+                        <div class="form-group">
+                            <label for="isi"><b>Isi Komentar</b></label>
+                            <textarea style="height: 200px" name="isi"
+                                class="form-control my-editor">{!! old('isi', $isi ?? '') !!}</textarea>
+                        </div>
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-outline-primary">Submit komentar</button>
+                        </div>
+                    </form>
                 </div>
-                    
             </div>
+
+        </div>
 
         <div class="col-md-2 mb-2">
             <div class="card main">
@@ -118,14 +115,14 @@
             </div>
         </div>
 
-</div>
+    </div>
 
-@endsection
+    @endsection
 
-@push('scripts')
-    
-<script>
-    var editor_config = {
+    @push('scripts')
+
+    <script>
+        var editor_config = {
     path_absolute : "/",
     selector: "textarea.my-editor",
     plugins: [
@@ -159,6 +156,6 @@
 };
 
 tinymce.init(editor_config);
-</script>
+    </script>
 
-@endpush
+    @endpush
--- resources/views/user/komentar/komentar_pertanyaan.blade.php
+++ resources/views/user/komentar/komentar_pertanyaan.blade.php
@@ -24,25 +24,23 @@
 
 ?>
 
-
+<!DOCTYPE HTML>
 @extends('layouts.app')
 
 @section('navbar')
-    <nav class="navbar navbar-light">
-        <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </nav>
+<nav class="navbar navbar-light">
+    <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+</nav>
 @endsection
 
 
 <style>
-
-    .card.main{
+    .card.main {
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     }
-
 </style>
 
 @section('content')
@@ -53,90 +51,91 @@
     </div>
 
     <div class="col-md-8 mb-2">
-            <div class="row justify-content-center">
-                <div class="col-md-12">
-                    <div class="card main">
-                        <div class="card-header"></div>
-                        
-                        <div class="card-body">
-                            {{-- @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
-                            @endif --}}
-                            <div class="card mb-2">
-                                <div class="card-header bg-warning">
-                                    Dari : {{$data_user->name}}
-                                </div>
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-10 col-sm-12">
-                                            <h5 class="card-title" style="font-weight: bold">{{$data_tanya->judul}}</h5>
-        
-                                            <p p class="card-text">{!!$data_tanya->isi!!}</p>
-                                            <div class="tag">
-                                                <?php
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card main">
+                    <div class="card-header"></div>
+
+                    <div class="card-body">
+                        {{-- @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                        @endif --}}
+                        <div class="card mb-2">
+                            <div class="card-header bg-warning">
+                                Dari : {{$data_user->name}}
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-10 col-sm-12">
+                                        <h5 class="card-title" style="font-weight: bold">{{$data_tanya->judul}}</h5>
+
+                                        <p p class="card-text">{!!$data_tanya->isi!!}</p>
+                                        <div class="tag">
+                                            <?php
                                                 
                                                     $tag = Pertanyaan_Tag::where('pertanyaan_id', $data_tanya->id)
                                                                             ->get();
                                                                             
                                                 ?>
-                                                @foreach ($tag as $tag_id)
-                                                    <?php
+                                            @foreach ($tag as $tag_id)
+                                            <?php
                                                         $tag_name = Tag::find($tag_id->tag_id);
                                                     ?>
-                                                        <button type="button" class="btn btn-info">{{$tag_name->nama_tag}}</button>
-                                                    
-                                                @endforeach
-                                            </div>
+                                            <button type="button" class="btn btn-info">{{$tag_name->nama_tag}}</button>
+
+                                            @endforeach
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            
                         </div>
-                            
-                        </div>
-                    </div>
-                </div>
-                <div class="card mt-2">
-                    <div class="card-header bg-info">
-                        Komentar Kamu
-                    </div>
-                    <div class="card-body">
-                        <form method="post" action="{{url('/komen-tanya')}}">
-                            @csrf
-                            <input type="hidden" name="created_at" value="{{$current_date_time}}">
-                            <input type="hidden" name="updated_at" value="{{$current_date_time}}">
-                            <input type="hidden" name="user_id" value="{{Auth::id()}}">
-                            <input type="hidden" name="pertanyaan_id" value="{{$data_tanya->id}}">
-                            <div class="form-group">
-                                <label for="isi"><b>Isi Komentar</b></label>
-                                <textarea style="height: 200px" name="isi" class="form-control my-editor">{!! old('isi', $isi ?? '') !!}</textarea>
-                            </div>
-                            <div class="form-group text-center">
-                                <button type="submit" class="btn btn-outline-primary">Submit komentar</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                    
-            </div>
 
-        <div class="col-md-2 mb-2">
-            <div class="card main">
-                <div class="card-body">
-                    @include('layouts.partials.rightbar')
+                    </div>
+
                 </div>
             </div>
         </div>
+        <div class="card mt-2">
+            <div class="card-header bg-info">
+                Komentar Kamu
+            </div>
+            <div class="card-body">
+                <form method="post" action="{{url('/komen-tanya')}}">
+                    @csrf
+                    <input type="hidden" name="created_at" value="{{$current_date_time}}">
+                    <input type="hidden" name="updated_at" value="{{$current_date_time}}">
+                    <input type="hidden" name="user_id" value="{{Auth::id()}}">
+                    <input type="hidden" name="pertanyaan_id" value="{{$data_tanya->id}}">
+                    <div class="form-group">
+                        <label for="isi"><b>Isi Komentar</b></label>
+                        <textarea style="height: 200px" name="isi"
+                            class="form-control my-editor">{!! old('isi', $isi ?? '') !!}</textarea>
+                    </div>
+                    <div class="form-group text-center">
+                        <button type="submit" class="btn btn-outline-primary">Submit komentar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col-md-2 mb-2">
+        <div class="card main">
+            <div class="card-body">
+                @include('layouts.partials.rightbar')
+            </div>
+        </div>
+    </div>
 
 </div>
 
 @endsection
 
 @push('scripts')
-    
+
 <script>
     var editor_config = {
     path_absolute : "/",
